9f4b85d7198373e1d1183410c5401b4820439c40
gayathri1011 <gayathri.r.s.10e@gmail.com>
2026-09-04 11:32:54 +0530

    Add files via upload

---
+++ DSA 2.py
@@ -0,0 +1,34 @@
+Write a program which takes two values x and y. Prints x for y number of times.
+
+a = int(input("enter x value = "))
+b = int(input("enter y value = "))
+for i in range(b):
+  print(a)
+
+
+Write a program to take x and print multiples of x till 1000.
+
+a = int(input("enter x = "))
+
+i = a
+
+while i <= 1000:
+    print(i)
+    i = i + a
+
+Write a program to get firstName and lastName and n as input and print fullName that is firstName+lastName for n times.
+
+a = input("enter the first name ")
+b = input("enter the last name ")
+c = int(input("enter n "))
+
+i = 0
+
+while(i < c):
+  print(a+b)
+
+  
+
+  
+
+
